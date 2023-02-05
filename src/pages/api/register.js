8e202cad2f755bf3f5b3a16e170c7e9eb27aa81d
seansystem6 <123790397+seansystem6@users.noreply.github.com>
2023-02-05 14:19:58 +0800

    fixed registration bug

--- src/pages/api/register.js
+++ src/pages/api/register.js
@@ -35,9 +35,9 @@ async function handler(req, res) {
             const hashedPassword = await hashPassword(password);
 
             const result = await db.collection("users").insertOne({
-                email: email,
+                email: email.trim(),
                 password: hashedPassword,
-                name: name,
+                name: name.trim(),
             });
 
             if (!result) {
